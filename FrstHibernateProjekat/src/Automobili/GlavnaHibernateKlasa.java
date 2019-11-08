5dec559db3cce2fba51612715ac1757d5687f26f
okkursevi4 <okkursevi4@UC5PC9>
2019-11-08 20:02:02 +0100

    Hibernate metode za select, update i delate

--- FrstHibernateProjekat/src/Automobili/GlavnaHibernateKlasa.java
+++ FrstHibernateProjekat/src/Automobili/GlavnaHibernateKlasa.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import Controler.HibernateDAO;
 import Model.Car;
 
 public class GlavnaHibernateKlasa {
@@ -11,40 +12,43 @@ public class GlavnaHibernateKlasa {
 	public static void main(String[] args) {
 		
 		
-SessionFactory factory = new Configuration().configure().buildSessionFactory();
-
-
-	Car car = new Car("bugatti", "veyron", 2018, 1.8 , true);
-	
-
-
+//SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
 //sesija se otvora na openSession(); - ovo aktivira hibernate
-Session sesija = factory.openSession();
+//Session sesija = factory.openSession();
 //svaka sesija zapocinje neku transakciju
-	sesija.beginTransaction();
+	//sesija.beginTransaction();
 	
 	//ako transakcija sve uradi onda ce napraviti commit 
 	//ako transakcija ne usepe bice rollback
 
-	try {
+	//try {
 		//na ovaj nacin cuvamo ceo objekat u bazi
-		sesija.save(car);
-		System.out.println("Ubacen objekat u bazu ");
-		sesija.getTransaction().commit();
-	} catch (Exception e) {
-		sesija.getTransaction().rollback();
-	}
+		//sesija.save(car);
+		//System.out.println("Ubacen objekat u bazu ");
+		//sesija.getTransaction().commit();
+	//} catch (Exception e) {
+		//sesija.getTransaction().rollback();
+	//}
 
 //sesija se zatvara kao i svaka druga metoda .close();
-sesija.close();
+//sesija.close();
 		
 		
+		HibernateDAO dao = new HibernateDAO();
+		//Car car = new Car("maserati", "ghilbi", 2017, 0.18 , true);
+		//dao.snimiAutoUBazu(car);
 		
+		/*Car car = dao.vratiAutoPoID(2);
+		dao.updateCarPrice(car.getIdCar(), 180000);
+		System.out.println("Uzeo si auto " + car.getMarka() + " " + car.getModel());
+		System.out.println("Nova cena je " + car.getCena());*/
 		
-		
-		
-		
+		if (dao.delateCar(1)) {
+			System.out.println("Obrisan je auto ");
+		} else {
+			System.out.println("Auto nije obrisan ");
+		}
 		
 		
 		
---
+++ FrstHibernateProjekat/src/Controler/HibernateDAO.java
@@ -0,0 +1,112 @@
+package Controler;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import Model.Car;
+
+public class HibernateDAO {
+	
+	SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	
+	
+	public boolean snimiAutoUBazu(Car car) {
+		
+		Session sesija = factory.openSession();
+			sesija.beginTransaction();
+			
+			try {
+				sesija.save(car);
+				sesija.getTransaction().commit();
+				System.out.println();
+				return true;
+			} catch (Exception e) {
+				sesija.getTransaction().rollback();
+				return false;
+			} finally {
+				sesija.close();
+			}
+	}
+	
+	public Car vratiAutoPoID (int id) {
+		
+		Session sesija = factory.openSession();
+		sesija.beginTransaction();
+		//dodeljujemo vrednost objektu null;
+		Car car = null;
+		try {
+			//ovo pravimo da nam objekat ne bi vratio nall;
+			car = sesija.get(Car.class, id);
+			sesija.getTransaction().commit();
+			System.out.println();
+			return car;
+		} catch (Exception e) {
+			sesija.getTransaction().rollback();
+			return null;
+			//moze i return car; 
+		} finally {
+			sesija.close();
+		}
+	}
+	
+	public void updateCarPrice (int id, double novaCena) {
+		
+		Session sesija = factory.openSession();
+		sesija.beginTransaction();
+		//dodeljujemo vrednost objektu null;
+		Car car = null;
+		try {
+			//ovo pravimo da nam objekat ne bi vratio nall;
+			car = sesija.get(Car.class, id);
+			car.setCena(novaCena);
+			sesija.update(car);
+			sesija.getTransaction().commit();
+			System.out.println();
+		
+		} catch (Exception e) {
+			sesija.getTransaction().rollback();
+		
+		} finally {
+			sesija.close();
+		}
+	}
+	
+	public boolean delateCar (int id) {
+	
+	Session sesija = factory.openSession();
+	sesija.beginTransaction();
+	//dodeljujemo vrednost objektu null;
+	Car car = null;
+	try {
+		car = sesija.get(Car.class, id);
+		sesija.delete(car);
+		sesija.getTransaction().commit();
+		System.out.println();
+		return true;
+	
+	} catch (Exception e) {
+		sesija.getTransaction().rollback();
+		return false;
+		
+	} finally {
+		sesija.close();
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
